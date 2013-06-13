1e88a6f0baa6d70cfe5dfa50536e67e2f7f6b677
Alexandre Morignot <erdnaxeli@gmail.com>
2013-06-13 05:11:04 +0200

    We will certainly need the screen in the futur

--- event.h
+++ event.h
@@ -2,6 +2,6 @@
 #define EVENT_H
 
 void subscribe_events(xcb_connection_t *c, xcb_drawable_t root);
-void event_handler(xcb_connection_t *c, xcb_drawable_t root);
+void event_handler(xcb_connection_t *c, xcb_screen_t *scren, xcb_drawable_t root);
 
 #endif //EVENT_H
