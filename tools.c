e0ffbf377d48278cfd4b136e4ea53132ffa130d6
Alexandre Morignot <erdnaxeli@gmail.com>
2013-06-16 19:37:56 +0200

    log levels

--- tools.c
+++ tools.c
@@ -2,13 +2,18 @@
 #include <unistd.h>
 #include <signal.h>
 
+#include "logger.h"
+
 pid_t exec_cmd(const char *cmd)
 {
+    logger(DEBUG, "exec_cmd: %s", cmd);
+
     pid_t pid;
     pid = fork();
 
     if (pid == 0) {
         execlp("/bin/sh", "sh", "-c", cmd, NULL);
+        logger(CRITICAL, "exec_cmd: execld failed");
         exit(EXIT_FAILURE); // we should never reach this line
     }
 
--- window.c
+++ window.c
@@ -10,35 +10,35 @@ extern struct conf global;
 struct split_state *get_lnode(struct split_state *tree, xcb_window_t *w)
 {
     struct split_state *tmp;
-    logger(DEBUG, "get_lnode: searching for %d", *w);
+    logger(TRACE, "get_lnode: searching for %d", *w);
 
     if (tree->right == NULL) { // tree->left == NULL too
-        logger(DEBUG, "get_lnode: end of the tree");
+        logger(TRACE, "get_lnode: end of the tree");
 
         if (tree->v == NULL) {
-            logger(DEBUG, "get_lnode: window not found");
+            logger(TRACE, "get_lnode: window not found");
             return tree;
         }
         else {
             xcb_window_t *win = (xcb_window_t *)tree->v;
-            logger(DEBUG, "get_lnode: testing %d", *win);
+            logger(TRACE, "get_lnode: testing %d", *win);
 
             if (*win == *w) {
-                logger(DEBUG, "get_lnode: window found");
+                logger(TRACE, "get_lnode: window found");
                 return tree;
             } else {
-                logger(DEBUG, "get_lnode: window not found");
+                logger(TRACE, "get_lnode: window not found");
                 return NULL;
             }
         }
     } else {
-        logger(DEBUG, "get_lnode: searching in childs");
+        logger(TRACE, "get_lnode: searching in childs");
         if ((tmp = get_lnode(tree->right, w)) != NULL ||
                     (tmp = get_lnode(tree->left, w)) != NULL) {
-            logger(DEBUG, "get_lnode: window found");
+            logger(TRACE, "get_lnode: window found");
             return tmp;
         } else {
-            logger(DEBUG, "get_lnode: window not found");
+            logger(TRACE, "get_lnode: window not found");
             return NULL;
         }
     }
@@ -49,7 +49,7 @@ struct split_state *get_lnode(struct split_state *tree, xcb_window_t *w)
  * @return a pointer to this list */
 struct wlist *add_win_to_list(struct wlist *list, xcb_window_t *w)
 {
-    logger(DEBUG, "add_win_to_list: entering");
+    logger(TRACE, "add_win_to_list");
     struct wlist *save = list;
 
     if (list == NULL) {
